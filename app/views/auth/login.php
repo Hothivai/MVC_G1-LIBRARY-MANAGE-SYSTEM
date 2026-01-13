39f783dba2762a95b5b820680b466ac0723b2aef
ThiMung <dtmung0712@gmail.com>
2026-01-13 09:28:16 +0700

    create login page UI

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -1,20 +1,38 @@
-<?php ?>
-<?php include '../app/views/layouts/header.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<section class="login">
-    <h1>Đăng Nhập</h1>
-    <form method="POST" action="/login">
-        <div class="form-group">
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
-        </div>
-        <div class="form-group">
-            <label for="password">Mật Khẩu:</label>
-            <input type="password" id="password" name="password" required>
-        </div>
-        <button type="submit" class="btn btn-primary">Đăng Nhập</button>
-    </form>
-    <p>Chưa có tài khoản? <a href="/register">Đăng ký tại đây</a></p>
-</section>
+<head>
+    <title>Login form</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="/public/css/login.css">
+</head>
 
-<?php include '../app/views/layouts/footer.php'; ?>
+<body>
+    <?php include './app/views/layouts/header.php'; ?>
+    <div class="container mt-3">
+        <img src="/public/images/logo.png" alt="Library logo" class="logo">
+        <h5>LIBRARY MANAGEMENT SYSTEM</h5>
+        <form action="/action_page.php">
+            <div class="mb-3 mt-3">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+            </div>
+            <div class="mb-3">
+                <label for="pwd">Password</label>
+                <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+            </div>
+            <button type="submit" class="btn">LOG IN</button>
+            <div class="register mt-3">
+                <label for="text">Don't have an account?</label>
+                <a href="./register.php" class="register-link">Register now</a>
+            </div>
+        </form>
+    </div>
+
+    <?php include './app/views/layouts/footer.php'; ?>
+
+</body>
+</html>
