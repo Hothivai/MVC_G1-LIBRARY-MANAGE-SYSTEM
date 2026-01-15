3e193d87c5ebb62c96c4928eda72f327fd8c4af0
ThiMung <dtmung0712@gmail.com>
2026-01-15 11:22:15 +0700

    update connect-db

--- config/config.php
+++ config/config.php
@@ -6,10 +6,10 @@
 
 	try {
 		// Create PDO connection
-		$conn = new PDO("mysql:host=$host;dbname=$database;charset=utf8", $user, $password);
+		$db = new PDO("mysql:host=$host;dbname=$database;charset=utf8", $user, $password);
 		
 		// Set error mode to exception
-		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		
 		//echo "Connected successfully";
 	} catch(PDOException $e) {
