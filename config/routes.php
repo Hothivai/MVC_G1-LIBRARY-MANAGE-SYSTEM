36c9e96475660820e4f762f1300c33bfe2bc17fa
ALangThiThom <alangthithom202@gmail.com>
2026-01-18 15:26:30 +0700

    LMS-7: Add login routes

--- config/routes.php
+++ config/routes.php
@@ -5,3 +5,7 @@ $router = new Router();
 // REGISTER
 $router->get('/register', 'AuthController', 'register');
 $router->post('/register', 'AuthController', 'handleRegister');
+
+// LOGIN
+$router->get('/login', 'AuthController', 'login');
+$router->post('/login', 'AuthController', 'handleLogin');
